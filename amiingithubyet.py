75f6ec7e4eff7fcc2195f9b15ac947c304d11840
Marcin Jastrzebski <marcinjastrzebski8@gmail.com>
2020-03-31 16:38:55 +0100

    another comment

--- amiingithubyet.py
+++ amiingithubyet.py
@@ -2,3 +2,9 @@
 """ok not in github yet but I am getting a feel for the repositories and branches
 
 YOU CAN SWITCH BETWEEN BRANCHES WITH git checkout <branch>"""
+
+"""
+To apply ideas from one branch to the master branch use git merge but not sure how to do this if my main one 
+isnt called master
+Should be doable though
+"""
